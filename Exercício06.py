bc0cf6ba90c5bb15c20de4a2b7fc568d2f073bbf
Felipe305 <felipe.profissionalti@gmail.com>
2021-08-30 20:43:51 -0300

    Concluído

---
+++ Exercício06.py
@@ -0,0 +1,15 @@
+import math
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.#
+
+a= int(input("Digite o raio do círculo:"))
+a = format(math.pi*pow(a,2),".4f")
+print(f"A área do circulo é:{a}")
+
+
+
+
+
+
+
+
+
