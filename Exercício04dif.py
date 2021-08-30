2738fc7106b5bcc5f018d60e44d64565c1908e00
Felipe305 <felipe.profissionalti@gmail.com>
2021-08-29 23:36:48 -0300

    Concluído

---
+++ Exercício04dif.py
@@ -0,0 +1,22 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.#
+
+notas = 0
+
+
+for i in range(4):
+    notas += int(input("Digite uma nota:"))
+
+print(f"A média é: {notas/4}")
+    
+
+
+
+
+
+
+
+
+
+
+
+
