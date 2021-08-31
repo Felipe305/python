be3f3d29e34251d2c64dd2dd7d2a327039a0f102
Felipe305 <felipe.profissionalti@gmail.com>
2021-08-31 20:34:23 -0300

    Concluído

---
+++ Exercício09.py
@@ -0,0 +1,7 @@
+import math 
+#Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius#
+
+graus_fahrenheit = int(input("Digite a temperatura em graus Fahrenheit:"))
+print(f"A temperatura é: {(graus_fahrenheit-32)/1.8}°C")
+
+
