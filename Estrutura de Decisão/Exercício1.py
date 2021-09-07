10301f46644aa01b27ad45f8d0f73331ea6e9233
Felipe Silva <felipe.profissionalti@gmail.com>
2021-09-07 11:06:30 -0300

    Conluido

---
+++ Estrutura de Decisão/Exercício1.py
@@ -0,0 +1,9 @@
+#Faça um Programa que peça dois números e imprima o maior deles.
+
+nmr1 = float(input("Digite um número:"))
+nmr2 = float(input("Digite outro número:"))
+
+if nmr1 > nmr2 :
+    print(f"O maior número é: {nmr1}")
+else:
+    print(f"O maior número é:{nmr2}")
---
+++ Estrutura de Decisão/Exercício2.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+nmr = float(input("Digite um número:"))
+
+if nmr > 0:
+    print("O número é positivo")
+else:
+    print("O número é negativo")
