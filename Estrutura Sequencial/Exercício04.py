4adea030ffde3cd29148dac41080b2d08f9f6773
Felipe Silva <felipe.profissionalti@gmail.com>
2021-09-08 21:49:49 -0300

    Concluido

--- Estrutura Sequencial/Exercício04.py
+++ Estrutura Sequencial/Exercício04.py
@@ -4,8 +4,12 @@ notas01 = int(input("Digite uma nota:"))
 notas02 = int(input("Digite uma nota:"))
 notas03 = int(input("Digite uma nota:"))
 notas04 = int(input("Digite uma nota:"))
-print(f"A média é: {(notas01+notas02+notas03+notas04)/4}")
+media = (notas01+notas02+notas03+notas04)/4
 
+if notas01 <= 10 and notas02 <= 10 and notas03 <= 10 and notas04 <= 10:
+    print("A média é: {}".format(media))
+else:
+    print("Foi informado alguma nota errada")
 
 
 
