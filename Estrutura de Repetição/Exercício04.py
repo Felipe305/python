98925741cd3d983887e226087ad36de3edc819f0
Felipe Silva <felipe.profissionalti@gmail.com>
2021-09-21 20:54:54 -0300

    Concluido

---
+++ Estrutura de Repetição/Exercício04.py
@@ -0,0 +1,17 @@
+#Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa anual de crescimento de 3% e que a população
+#de B seja 200000 habitantes com uma taxa de crescimento de 1.5%. Faça um programa que calcule e escreva o número de anos necessários 
+#para que a população do país A ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento.
+
+populacaoA = 80000
+crescimentoA = 1.03
+populacaoB = 200000
+crescimentoB = 0.015
+
+ano = 1
+while(populacaoA <= populacaoB):
+    populacaoA *= crescimentoA
+    populacaoB *= crescimentoB
+    ano += 1
+
+    print("Sera necessarios, {} anos para a população do pais A " .format(ano))
+    print("Ultrapassar a população do pais B")
